db57f0a3632d2c03b5c449745bf80a4759beef9f
Magnus Grimstvedt Saltnes <magnussaltnes@gmail.com>
2022-05-30 12:19:34 +0200

    Improve automatic Stanek management.

--- src/stanekManager.js
+++ src/stanekManager.js
@@ -2,8 +2,8 @@
 
 export async function main(ns) {
 	while(true){
-		const fragments = ns.stanek.activeFragments();
+		const fragments = ns.stanek.activeFragments().filter(f => f.id < 100 && (f.numCharge > 0));
 		const minCharged = fragments.sort((a,b) => a.numCharge - b.numCharge)[0];
-		await ns.stanek.charge(minCharged.x, minCharged.y);
+		await ns.stanek.chargeFragment(minCharged.x, minCharged.y);
 	}
 }
