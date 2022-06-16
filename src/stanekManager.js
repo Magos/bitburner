ae63a3afeeaa5850084dcd5446cb7e74ec991de6
Magnus Grimstvedt Saltnes <magnussaltnes@gmail.com>
2022-06-16 09:16:39 +0200

    Dedicate 'home' to stanek if it is being used

--- src/stanekManager.js
+++ src/stanekManager.js
@@ -1,9 +1,11 @@
 /** @param {NS} ns **/
-
+let startTime = new Date();
+let now = new Date();
 export async function main(ns) {
-	while(true){
-		const fragments = ns.stanek.activeFragments().filter(f => f.id < 100 && (f.numCharge > 0));
+	while(now - startTime > 3600000){ ///Run for 1 hour at a time.
+		const fragments = ns.stanek.activeFragments().filter(f => f.id < 100);
 		const minCharged = fragments.sort((a,b) => a.numCharge - b.numCharge)[0];
 		await ns.stanek.chargeFragment(minCharged.x, minCharged.y);
+		now = new Date();
 	}
 }
