30fe5b609ea0aa693442c03bbda742b2239a5729
Magnus Grimstvedt Saltnes <magnussaltnes@gmail.com>
2022-06-16 18:28:58 +0200

    Fixes to the new stanek approach.

--- src/stanekManager.js
+++ src/stanekManager.js
@@ -2,7 +2,7 @@
 let startTime = new Date();
 let now = new Date();
 export async function main(ns) {
-	while(now - startTime > 3600000){ ///Run for 1 hour at a time.
+	while(now - startTime < 3600000){ ///Run for 1 hour at a time.
 		const fragments = ns.stanek.activeFragments().filter(f => f.id < 100);
 		const minCharged = fragments.sort((a,b) => a.numCharge - b.numCharge)[0];
 		await ns.stanek.chargeFragment(minCharged.x, minCharged.y);
